f5b3b3392f0832f5ee9d8f281b26cf3b2291128e
SevarJafarli <Sevar.Jafarli@abb-bank.az>
2026-06-10 23:19:16 +0400

    add domain entities layer

---
+++ CurrencyConverter/CurrencyConverter/Domain/Entities/ExchangeRate.swift
@@ -0,0 +1,24 @@
+//
+//  ExchangeRate.swift
+//  CurrencyConverter
+//
+//  Created by Sevar Jafarli on 10.06.26.
+//
+
+import Foundation
+
+struct ExchangeRate: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    let currencies: [Currency]
+    
+    init(id: UUID, date: Date, currencies: [Currency]) {
+        self.id = id
+        self.date = date
+        self.currencies = currencies
+    }
+    
+    func currency(for code: String) -> Currency? {
+        currencies.first { $0.code == code }
+    }
+}
