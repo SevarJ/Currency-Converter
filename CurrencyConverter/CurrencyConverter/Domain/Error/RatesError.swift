a3be2353591b638835522e8bf4de03a35905b225
SevarJafarli <Sevar.Jafarli@abb-bank.az>
2026-06-11 08:29:32 +0400

    add domain

---
+++ CurrencyConverter/CurrencyConverter/Domain/Error/RatesError.swift
@@ -0,0 +1,13 @@
+//
+//  RatesError.swift
+//  CurrencyConverter
+//
+//  Created by Sevar Jafarli on 11.06.26.
+//
+
+enum RatesError: Error {
+    case networkUnavailable
+    case parsingFailed
+    case cacheEmpty
+    
+}
