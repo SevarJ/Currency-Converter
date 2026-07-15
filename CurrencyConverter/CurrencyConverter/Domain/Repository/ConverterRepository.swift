fcf2b574ab49f9dcebe8991752499369b71745ba
SevarJafarli <Sevar.Jafarli@abb-bank.az>
2026-07-15 11:34:09 +0400

    move to seperate file

---
+++ CurrencyConverter/CurrencyConverter/Domain/Repository/ConverterRepository.swift
@@ -0,0 +1,34 @@
+//
+//  ConverterRepository.swift
+//  CurrencyConverter
+//
+//  Created by Sevar Jafarli on 15.07.26.
+//
+
+import Foundation
+
+final class ConverterRepository: ConverterRepositoryProtocol {
+    
+    private let networkService: NetworkServiceProtocol
+    
+    init(
+        networkService: NetworkServiceProtocol = ConverterNetworkService()
+    ) {
+        self.networkService = networkService
+    }
+    
+    func currencies() async throws -> [Currency] {
+        let dtos: [CurrencyDTO] = try await networkService.fetch(.currencies)
+        return dtos.map { $0.toDomain() }
+    }
+    
+    func rate(base: String, quote: String) async throws -> Rate {
+        let dto: RateDTO = try await networkService.fetch(.rate(base: base, quote: quote))
+        return try dto.toDomain()
+    }
+    
+    func rates(base: String, quotes: [String]) async throws -> [Rate] {
+        let dtos: [RateDTO] = try await networkService.fetch(.rates(base: base, quotes: quotes))
+        return try dtos.map { try $0.toDomain() }
+    }
+}
