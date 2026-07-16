97bfa309a3ea5a4982a8feb9fb14babb5c17e323
SevarJafarli <Sevar.Jafarli@abb-bank.az>
2026-07-16 09:37:23 +0400

    add history endpoint

--- CurrencyConverter/CurrencyConverter/Data/Network/Endpoint/ConverterEndpoint.swift
+++ CurrencyConverter/CurrencyConverter/Data/Network/Endpoint/ConverterEndpoint.swift
@@ -11,6 +11,7 @@ enum ConverterEndpoint {
     case currencies
     case rate(base: String, quote: String)
     case rates(base: String, quotes: [String])
+    case history(base: String, quote: String, from: Date, to: Date?, group: String?)
     
     var url: URL? {
         var urlComponents = URLComponents()
@@ -31,6 +32,30 @@ enum ConverterEndpoint {
             
             if !quotes.isEmpty {
                 urlComponents.queryItems?.append(URLQueryItem(name: "quotes", value: quotes.joined(separator: ",")))
+            }
+            
+        case let .history(base, quote, from, to, group):
+            urlComponents.path = "/v2/rates"
+            urlComponents.queryItems = [
+                URLQueryItem(name: "base", value: base),
+                URLQueryItem(name: "quotes", value: quote),
+                URLQueryItem(name: "from", value: DateUtils.dateFormatter.string(from: from))
+            ]
+            
+            if let to {
+                urlComponents.queryItems?.append(URLQueryItem(
+                    name: "to",
+                    value: DateUtils.dateFormatter.string(from: to))
+                )
+            }
+            
+            if let group {
+                urlComponents.queryItems?.append(
+                    URLQueryItem(
+                        name: "group",
+                        value: group
+                    )
+                )
             }
         }
         
--- CurrencyConverter/CurrencyConverter/Domain/Repository/ConverterRepositoryProtocol.swift
+++ CurrencyConverter/CurrencyConverter/Domain/Repository/ConverterRepositoryProtocol.swift
@@ -5,8 +5,11 @@
 //  Created by Sevar Jafarli on 15.07.26.
 //
 
+import Foundation
+
 protocol ConverterRepositoryProtocol {
     func currencies() async throws -> [Currency]
     func rate(base: String, quote: String) async throws -> Rate
     func rates(base: String, quotes: [String]) async throws -> [Rate]
+    func history(base: String, quote: String, from: Date, to: Date?) async throws -> [Rate]
 }
